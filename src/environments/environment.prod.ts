7fd2d2117f45cffa074abb0674ffe621dadcf471
Arun <arun3050@gmail.com>
2018-07-07 00:15:52 +0530

    simple authentication and some routing setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyD7-t9R_qTkq6YO1tfuhG98BKZ2o44U9qE',
+    authDomain: 'oshop-proj.firebaseapp.com',
+    databaseURL: 'https://oshop-proj.firebaseio.com',
+    projectId: 'oshop-proj',
+    storageBucket: 'oshop-proj.appspot.com',
+    messagingSenderId: '58436156231'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyD7-t9R_qTkq6YO1tfuhG98BKZ2o44U9qE',
+    authDomain: 'oshop-proj.firebaseapp.com',
+    databaseURL: 'https://oshop-proj.firebaseio.com',
+    projectId: 'oshop-proj',
+    storageBucket: 'oshop-proj.appspot.com',
+    messagingSenderId: '58436156231'
+  }
 };
 
 /*
